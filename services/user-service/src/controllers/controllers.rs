ee00d840e3d0b01c4c3b91369d290ad2002c4ceb
Sanjay Bhattacharya <sanjbh@gmail.com>
2025-07-23 10:21:36 +0530

    Fixed routes

--- services/user-service/src/controllers/controllers.rs
+++ services/user-service/src/controllers/controllers.rs
@@ -1,36 +1,84 @@
-use rocket::{State, get, post, serde::json::Json};
-use shared_common::responses::{ApiResponse, ApiResult};
+use rocket::{State, get, post, routes, serde::json::Json};
+use shared_auth::guards::AuthenticatedUser;
+use shared_common::responses::ApiResponse;
 
 use crate::{
     models::{CreateUserRequest, LoginRequest, LoginResponse, UserResponse},
     service::UserService,
 };
 
+// #[post("/register", data = "<request>")]
+// pub async fn register(
+//     user_service: &State<UserService>,
+//     request: Json<CreateUserRequest>,
+// ) -> ApiResult<Json<ApiResponse<UserResponse>>> {
+//     let result = user_service.register_user(request.into_inner()).await;
+
+//     match result {
+//         Ok(user) => Ok(Json(ApiResponse::success(user))),
+//         Err(e) => Ok(Json(ApiResponse::error(e.to_string()))),
+//     }
+// }
+
 #[post("/register", data = "<request>")]
 pub async fn register(
-    user_service: &State<UserService>,
+    user_state: &State<UserService>,
     request: Json<CreateUserRequest>,
-) -> ApiResult<Json<ApiResponse<UserResponse>>> {
-    let result = user_service.register_user(request.into_inner()).await;
+) -> Json<ApiResponse<UserResponse>> {
+    let result = user_state.register_user(request.into_inner()).await;
 
-    match result {
-        Ok(user) => Ok(Json(ApiResponse::success(user))),
-        Err(e) => Ok(Json(ApiResponse::error(e.to_string()))),
-    }
+    Json(
+        result
+            .map(ApiResponse::success)
+            .unwrap_or_else(|e| ApiResponse::error(e.to_string())),
+    )
 }
+
+// #[post("/login", data = "<request>")]
+// pub async fn login(
+//     user_service: &State<UserService>,
+//     request: Json<LoginRequest>,
+// ) -> ApiResult<Json<ApiResponse<LoginResponse>>> {
+//     let result = user_service.login(request.into_inner()).await;
+
+//     match result {
+//         Ok(response) => Ok(Json(ApiResponse::success(response))),
+//         Err(e) => Ok(Json(ApiResponse::error(e.to_string()))),
+//     }
+// }
 
 #[post("/login", data = "<request>")]
 pub async fn login(
     user_service: &State<UserService>,
     request: Json<LoginRequest>,
-) -> ApiResult<Json<ApiResponse<LoginResponse>>> {
+) -> Json<ApiResponse<LoginResponse>> {
     let result = user_service.login(request.into_inner()).await;
 
-    match result {
-        Ok(response) => Ok(Json(ApiResponse::success(response))),
-        Err(e) => Ok(Json(ApiResponse::error(e.to_string()))),
-    }
+    let return_value = result
+        .map(ApiResponse::success)
+        .unwrap_or_else(|e| ApiResponse::error(e.to_string()));
+
+    Json(return_value)
 }
 
 // #[get("/profile")]
 // pub async fn get_profile(user_service: &State<UserService>, user: Auth)
+
+#[get("/profile")]
+pub async fn get_profile(
+    user_service: &State<UserService>,
+    user: AuthenticatedUser,
+) -> Json<ApiResponse<UserResponse>> {
+    let result = user_service.get_user_profile(user.id).await;
+
+    Json(
+        result
+            .map(ApiResponse::success)
+            .unwrap_or_else(|e| ApiResponse::error(e.to_string())),
+    )
+    // unimplemented!()
+}
+
+pub fn routes() -> Vec<rocket::Route> {
+    routes![register, login, get_profile]
+}
--- shared/auth/src/guards/guards.rs
+++ shared/auth/src/guards/guards.rs
@@ -1,9 +1,11 @@
 use rocket::{
     Request,
+    http::Status,
     request::{FromRequest, Outcome},
-    tokio,
 };
 use uuid::Uuid;
+
+use crate::jwt::verify_access_token;
 
 #[derive(Debug)]
 pub struct AuthenticatedUser {
@@ -15,5 +17,33 @@ pub struct AuthenticatedUser {
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for AuthenticatedUser {
     type Error = &'static str;
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {}
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth_header = match request.headers().get_one("Authorization") {
+            Some(header) => header,
+            None => {
+                return Outcome::Error((Status::Unauthorized, "Authorization header not present"));
+            }
+        };
+
+        if !auth_header.starts_with("Bearer ") {
+            return Outcome::Error((Status::Unauthorized, "Invalid authorization format"));
+        }
+
+        let token = &auth_header[7..];
+
+        match verify_access_token(token) {
+            Ok(claim) => {
+                let id = match Uuid::parse_str(&claim.sub) {
+                    Ok(id) => id,
+                    Err(e) => return Outcome::Error((Status::Unauthorized, "Invalid user id")),
+                };
+                Outcome::Success(AuthenticatedUser {
+                    id,
+                    email: claim.email,
+                    user_type: claim.user_type,
+                })
+            }
+            Err(_) => return Outcome::Error((Status::Unauthorized, "Invalid token")),
+        }
+    }
 }
